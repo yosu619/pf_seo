52496c29e2c478dac1c0c59cc3ea04095db3f5e2
yosu619 <s.yousuke@outlook.jp>
2025-03-16 20:31:32 +0900

    pf_upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,7 @@
-// .s_01 .accordion_one
-$(function(){
-  //.accordion_oneの中の.accordion_headerがクリックされたら
-  $('.s_01 .accordion_one .accordion_header').click(function(){
-    //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
-    $(this).next('.accordion_inner').slideToggle();
-    $(this).toggleClass("open");
+$(function () {
+  $(".plus-btn").click(function () {
+    // クリックされた `.plus-btn` の親要素内の `.accordion__inner` のみ開閉
+    $(this).siblings(".accordion__inner").slideToggle();
+    $(this).toggleClass("open"); // ボタンの見た目を変更（開閉状態の切り替え）
   });
 });
